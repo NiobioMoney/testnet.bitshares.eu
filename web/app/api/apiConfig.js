c7de75751e7fa3c8814ea41165c7af03667ebcb2
Fabian Schuh <Fabian@chainsquad.com>
2017-03-03 16:23:59 +0100

    [testnet] new api server location

--- web/app/api/apiConfig.js
+++ web/app/api/apiConfig.js
@@ -5,17 +5,17 @@ export const blockTradesAPIs = {
 };
 
 export const settingsAPIs = {
-    DEFAULT_WS_NODE: "wss://bitshares.openledger.info/ws",
+    DEFAULT_WS_NODE: "wss://node.testnet.bitshares.eu",
     WS_NODE_LIST: [
+        {url: "wss://node.testnet.bitshares.eu", location: "Public Testnet Server (Frankfurt, Germany)"},
         {url: "wss://bitshares.openledger.info/ws", location: "Nuremberg, Germany"},
         {url: "wss://eu.openledger.info/ws", location: "Berlin, Germany"},
         {url: "wss://bit.btsabc.org/ws", location: "Hong Kong"},
         {url: "wss://bts.transwiser.com/ws", location: "Hangzhou, China"},
         {url: "wss://bitshares.dacplay.org:8089/ws", location:  "Hangzhou, China"},
         {url: "wss://openledger.hk/ws", location: "Hong Kong"},
-        {url: "wss://secure.freedomledger.com/ws", location: "Toronto, Canada"},
-        {url: "wss://testnet.bitshares.eu/ws", location: "Public Testnet Server (Frankfurt, Germany)"}
+        {url: "wss://secure.freedomledger.com/ws", location: "Toronto, Canada"}
     ],
-    DEFAULT_FAUCET: "https://bitshares.openledger.info",
+    DEFAULT_FAUCET: "https://faucet.testnet.bitshares.eu",
     RPC_URL: "https://openledger.info/api/"
 };
